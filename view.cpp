1a8af58efcec213f0ec140012604c8c5ad0ea903
LinaNaitAtmane <naitatmanelinaa@gmail.com>
2026-04-08 11:41:18 +0200

    Add files via upload

--- view.cpp
+++ view.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-// D´efinir la fonction print square qui affiche une case.
+
 void print_square(Piece piece)
 {
     switch (piece)
@@ -51,53 +51,37 @@ void print_square(Piece piece)
     }
 }
 
-// D´efinir la fonction print board
 void print_board(Board T)
 {
-    // lettres en haut
     cout << "  a b c d e f g h" << endl;
-
     for (int i = 7; i >= 0; i--)
     {
-        // numero a gauche
         cout << i + 1 << " ";
-
         for (int j = 0; j < 8; j++)
-        {
             print_square(T[i][j]);
-        }
-
-        // numero a droite
-        cout << i + 1;
-
-        cout << endl;
+        cout << i + 1 << endl;
     }
-
-    // lettres en bas
     cout << "  a b c d e f g h" << endl;
 }
+
 void write_FEN(Board T, const string &filename)
 {
     ofstream file(filename);
-    // Verifier que le fichier est bien ouvert
     if (!file.is_open())
     {
         cout << "Erreur";
         return;
     }
-    // On parcourt le plateau de la  rangee 8 a la 1
     for (int i = 7; i >= 0; i--)
     {
-        // count compte le nombre de cases vides consécutives sur une rangée
         int count = 0;
-        // Parcourt les colonnes de gauche a droite pour chaque rangee
         for (int j = 0; j < 8; j++)
         {
             Piece piece = T[i][j];
             if (piece == vide)
                 count++;
             else
-            { // on écrit le nombre de cases vides précédentes puis on reinitialise a 0
+            {
                 if (count > 0)
                 {
                     file << count;
@@ -146,7 +130,6 @@ void write_FEN(Board T, const string &filename)
                 }
             }
         }
-        // a  la fin de la rangee, si des cases vides étaient en fin de rangée, on écrit ce nombre
         if (count > 0)
             file << count;
         if (i > 0)
@@ -164,24 +147,21 @@ void read_FEN(Board T, const string &filename)
         cout << "Erreur";
         return;
     }
-    // on a declare fen pour stocker la ligne FEN dans le fichier
     string fen;
-    // on lit une ligne depuis le fichier
     getline(file, fen);
     file.close();
 
     int i = 7, j = 0;
-    // c'est une boucle sur chaque caractere c de la chaine fen
     for (char c : fen)
     {
-        if (c == '/') // Quand on rencontre /, on descend d’une ligne (i--) et on repart à la première colonne (j = 0).
+        if (c == '/')
         {
             i--;
             j = 0;
         }
-        else if ((c >= '1') && (c <= '8')) // si c  est un chiffre on saute le nombre de cases
+        else if (c >= '1' && c <= '8')
         {
-            j += c - '0'; // il convertit un caractere en un entier
+            j += c - '0';
         }
         else
         {
@@ -230,76 +210,88 @@ void read_FEN(Board T, const string &filename)
         }
     }
 }
-void set_background(bool is_black)
+
+// -----------------------------------------------------------------------
+// Fond de case : deux tons marron
+//   case foncee : 94  (marron fonce)
+//   case claire : 180 (marron clair / beige)
+// -----------------------------------------------------------------------
+void set_background(bool is_dark_square)
 {
-    if (is_black)
-    {
-        cout << "\x1b[48;5;130m";
-    }
+    if (is_dark_square)
+        cout << "\x1b[48;5;94m";
     else
-    {
-        cout << "\x1b[48;5;215m";
-    }
+        cout << "\x1b[48;5;180m";
 }
+
+// -----------------------------------------------------------------------
+// Couleur du texte des pieces :
+//   pieces noires -> texte noir gras  \x1b[1;30m
+//   pieces blanches -> texte blanc vif \x1b[1;97m
+// Les lettres ASCII sont utilisees a la place des symboles Unicode
+// pour eviter les problemes d'affichage dans certains terminaux
+// -----------------------------------------------------------------------
 void set_foreground(Piece piece)
 {
     switch (piece)
     {
+    // Pieces noires : lettre minuscule, texte noir gras
     case r:
-        cout << "\x1b[30m\u265C";
-        break; // tour noire
+        cout << "\x1b[1;30m" << "r";
+        break;
     case n:
-        cout << "\x1b[30m\u265E";
-        break; // cavalier noir
+        cout << "\x1b[1;30m" << "n";
+        break;
     case b:
-        cout << "\x1b[30m\u265D";
-        break; // fou noir
+        cout << "\x1b[1;30m" << "b";
+        break;
     case q:
-        cout << "\x1b[30m\u265B";
-        break; // reine noire
+        cout << "\x1b[1;30m" << "q";
+        break;
     case k:
-        cout << "\x1b[30m\u265A";
-        break; // roi noir
+        cout << "\x1b[1;30m" << "k";
+        break;
     case p:
-        cout << "\x1b[30m\u265F";
-        break; // pion noir
-
+        cout << "\x1b[1;30m" << "p";
+        break;
+    // Pieces blanches : lettre majuscule, texte blanc vif
     case R:
-        cout << "\x1b[37m\u2656";
-        break; // tour blanche
+        cout << "\x1b[1;97m" << "R";
+        break;
     case N:
-        cout << "\x1b[37m\u2658";
-        break; // cavalier blanc
+        cout << "\x1b[1;97m" << "N";
+        break;
     case B:
-        cout << "\x1b[37m\u2657";
-        break; // fou blanc
+        cout << "\x1b[1;97m" << "B";
+        break;
     case Q:
-        cout << "\x1b[37m\u2655";
-        break; // reine blanche
+        cout << "\x1b[1;97m" << "Q";
+        break;
     case K:
-        cout << "\x1b[37m\u2654";
-        break; // roi blanc
+        cout << "\x1b[1;97m" << "K";
+        break;
     case P:
-        cout << "\x1b[37m\u2659";
-        break; // pion blanc
-
+        cout << "\x1b[1;97m" << "P";
+        break;
     case vide:
         cout << " ";
-        break; // case vide
+        break;
     }
 }
+
 void print_square_color(int i, int j, Piece T[8][8])
 {
-    bool is_black_square = (i + j) % 2 == 0; // case noire si somme des indices pair
-    set_background(is_black_square);
+    bool is_dark = (i + j) % 2 == 0;
+    set_background(is_dark);
+    cout << " "; // espace gauche
     set_foreground(T[i][j]);
-    cout << " ";       // espace pour bien encadrer la pièce
-    cout << "\x1b[0m"; // reset couleurs après la case
+    cout << " "; // espace droite
+    cout << "\x1b[0m";
 }
-// Affiche le plateau complet en couleur
+
 void print_board_color(Board T)
 {
-    cout << "  a  b  c  d  e  f  g  h" << endl;
+    cout << "   a  b  c  d  e  f  g  h" << endl;
     for (int i = 7; i >= 0; i--)
     {
         cout << i + 1 << " ";
@@ -307,43 +299,49 @@ void print_board_color(Board T)
             print_square_color(i, j, T);
         cout << " " << i + 1 << endl;
     }
-    cout << "  a  b  c  d  e  f  g  h" << endl;
+    cout << "   a  b  c  d  e  f  g  h" << endl;
 }
-// Affiche une case en tenant compte du masque
-// Affiche une case en tenant compte du masque
+
+// -----------------------------------------------------------------------
+// Affichage avec masque
+//   val = 0 -> marron normal
+//   val = 1 -> BLEU fonce   = deplacement possible
+//   val = 2 -> ROUGE        = prise possible
+//   val = 3 -> VERT         = piece adverse non attaquee
+// -----------------------------------------------------------------------
 void print_square_color(int i, int j, Piece T[8][8], Mask M)
 {
     int val = M[i][j];
 
     if (val == 0)
     {
-        /* Fond classique : cases orange alternées (échiquier normal) */
-        bool is_black_square = (i + j) % 2 == 0;
-        set_background(is_black_square);
+        bool is_dark = (i + j) % 2 == 0;
+        set_background(is_dark);
     }
     else if (val == 1)
-        cout << "\x1b[48;5;20m"; /* BLEU     = case libre (déplacement possible) */
+        cout << "\x1b[48;5;20m";
     else if (val == 2)
-        cout << "\x1b[48;5;196m"; /* ROUGE    = prise possible (pièce adverse)    */
+        cout << "\x1b[48;5;196m";
     else if (val == 3)
-        cout << "\x1b[48;5;28m"; /* VERT     = pièce non attaquée                */
+        cout << "\x1b[48;5;28m";
     else if (val == 4)
-        cout << "\x1b[48;5;130m"; /* MARRON   = autre information                 */
+        cout << "\x1b[48;5;130m";
     else if (val == 5)
-        cout << "\x1b[48;5;91m"; /* VIOLET   = autre information                 */
+        cout << "\x1b[48;5;91m";
     else if (val == 6)
-        cout << "\x1b[48;5;30m"; /* BLEU-VERT= pièces pouvant se déplacer        */
+        cout << "\x1b[48;5;30m";
     else if (val == 7)
-        cout << "\x1b[48;5;244m"; /* GRIS     = autre information                 */
+        cout << "\x1b[48;5;244m";
 
+    cout << " ";
     set_foreground(T[i][j]);
     cout << " ";
-    cout << "\x1b[0m"; /* reset couleurs */
+    cout << "\x1b[0m";
 }
-// Affiche le plateau complet avec masque
+
 void print_board_color(Board T, Mask M)
 {
-    cout << "  a  b  c  d  e  f  g  h" << endl;
+    cout << "   a  b  c  d  e  f  g  h" << endl;
     for (int i = 7; i >= 0; i--)
     {
         cout << i + 1 << " ";
@@ -351,5 +349,5 @@ void print_board_color(Board T, Mask M)
             print_square_color(i, j, T, M);
         cout << " " << i + 1 << endl;
     }
-    cout << "  a b c d e f g h" << endl;
+    cout << "   a  b  c  d  e  f  g  h" << endl;
 }
